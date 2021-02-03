2027defb9a7792f0a24c6bc877f62cb40137572b
sharma1234-creator <72900717+sharma1234-creator@users.noreply.github.com>
2021-02-03 14:14:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,8 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <h1 data-ns-test="project-name"> Abhishek kumar sharma</h1>
+    <p data-ns-test="project-description"> ytuiobnm,.sha,nan.an..ndn..,anoikmsbvbnmertyuiopghjkl;asdbnmcmc</p>
   )
 }
 
